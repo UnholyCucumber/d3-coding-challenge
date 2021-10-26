c81efe6e457e5aa6bb9155a6c5edb42da3af0bdb
Andy <AndyZhang@live.ca>
2021-10-26 00:46:09 -0400

    Fixed issue with svg sizing

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 var svgContainer = d3.select(".background")
     .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
 
 var platform = svgContainer.append("rect")
-    .attr("width", 10000)
-    .attr("height", 100)
+    .attr("width", "100%")
+    .attr("height", "10%")
     .attr("fill", 'orangered')
     .attr("position", "absolute")
-    .attr("bottom", 0)
+    .attr("bottom", 100)
     .attr("left", 0)
 
 
