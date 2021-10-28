7ab092af680d333e6da44354d6fd94e5ffc932db
Andy <AndyZhang@live.ca>
2021-10-27 22:03:20 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-//TO-DO:
-// restrict horizontal acceleration to only when on platform (don't apply when in air and only count when it's on platform)
-// higher jump if space is held
-// fall down
-// horizontal velocity to persist even after letting go
-// velocity bounceback from hitting things
 
-    // actual screen size
+// actual screen size
 var available_width = window.innerWidth,
     available_height = window.innerHeight,
 
-    // calculating params for base platform
+// calculating params for base platform
     base_platform_width = available_width * 1.00,
     base_platform_height = available_height * 0.10,
     base_platform_x = 0,
     base_platform_y = available_height - base_platform_height, // top of base platform
 
-    // calculating params for small platform
+// calculating params for small platform
     small_platform_width = available_width * 0.30,
     small_platform_height = available_height * 0.10,
     small_platform_x = available_width * 0.50 - small_platform_width/2, // left of small platform
@@ -23,24 +17,24 @@ var available_width = window.innerWidth,
     small_platform_y = available_height * 0.50, // top of small platform
     bottom_of_small_platform_y = small_platform_y + small_platform_height,
 
-    // calculating params for circle
+// calculating params for circle
     circle_radius = available_height * 0.05,
     circle_starting_x = available_width/2,
     circle_starting_y = base_platform_y - circle_radius,
     right_x_barrier = available_width - circle_radius,
     left_x_barrier = circle_radius,
     top_y_barrier = circle_radius,
-    bottom_y_barrier = base_platform_y - circle_radius;
+    bottom_y_barrier = base_platform_y - circle_radius,
 
+// turn on for console logs + red dot
     testing = false;
 
 var background_svg = d3.select(".background")
     .append("svg")
     .attrs({
         width: "100%",
-        height: "100%",
+        height: "100%"
     });
-
 
 var base_platform = background_svg.append("rect")
     .attrs({
@@ -51,6 +45,7 @@ var base_platform = background_svg.append("rect")
         x: base_platform_x
     })
 
+// red dot to show where things are visually
 if (testing){
     var dot = background_svg.append("circle")
         .attrs({
@@ -60,8 +55,6 @@ if (testing){
             cx: small_platform_right_x
         })
 }
-
-
 
 var small_platform = background_svg.append("rect")
     .attrs({
@@ -77,42 +70,51 @@ var circle = background_svg.append("circle")
         r: circle_radius,
         fill: "cyan",
         cx: circle_starting_x,
-        cy: circle_starting_y,
+        cy: circle_starting_y
     })
 
 $(function() {
 
     // initialize variables for moving circle
-    var x = circle_starting_x;
-    var y = circle_starting_y;
+    var x = circle_starting_x,
+        y = circle_starting_y;
 
     // variables to keep track of horizontal velocity + acceleration
-    var init_horizontal_acceleration = 1;
-    var horizontal_acceleration = 0.01;
-    var cur_horizontal_acceleration = init_horizontal_acceleration;
-    var init_horizontal_velocity_right = 2;
-    var init_horizontal_velocity_left = -2;
-    var horizontal_velocity = 0; // starting velocity
-    var friction = 0.25;
+        init_horizontal_acceleration = 1,
+        horizontal_acceleration = 0.01,
+        cur_horizontal_acceleration = init_horizontal_acceleration,
+        init_horizontal_velocity_right = 2, // when we first press right arrow
+        init_horizontal_velocity_left = -2, // when we first press left arrow
+        horizontal_velocity = 0, // starting velocity
+        friction = 0.25, // affects momentum (impacts how much circle moves
+                         // after key is unpressed)
 
     // variables to keep track of vertical velocity + acceleration
-    var jump_counter = 0;
-    var gravity = 0.05;
-    var vertical_velocity = 0;
-    var init_vertical_velocity = -4;
-    var jump_time = 0;
+        jump_counter = 0,
+        gravity = 0.05, // affect acceleration/deceleration of objects
+                        // while they're in air
+        vertical_velocity = 0, // starting velocity
+        init_vertical_velocity = -4, // when we first press jump
+        jump_time = 0, // tracks how long circle has been jumping
+                       // used for applying gravity to vertical velocity
 
-    var velocity_loss = 0.75;
-    var bounce_flag = false;
+        velocity_loss = 0.75, // affects how much velocity is preserved
+                              // when bouncing back after hitting surfaces
+        bounce_flag = false, // flag to feed into jump function to dictate
+                             // bounces
+        bounce_velocity_threshold = 3, // threshold for deciding when bounce is over
 
     // bools to check if key has been pressed
-    var leftPressed = false;
-    var rightPressed = false;
-    var spacePressed = false;
+        leftPressed = false,
+        rightPressed = false,
+        spacePressed = false;
 
     // keep looping while screen is active
     setInterval(function(){
 
+        // logic for calling jump function (affects vertical axis)
+
+        // for when spacebar is first pressed
         if((spacePressed || jump_time > 0)&&!bounce_flag){
             if(jump_counter == 1){
                 jump_time = 0;
@@ -120,16 +122,22 @@ $(function() {
             }
             jump_time += 0.1;
             jump_up_and_down();
-        // for bouncing
+        // for bounces
         } else if (Math.abs(vertical_velocity) > 0) {
             jump_time += 0.1;
             jump_up_and_down();   
         }
 
+        // logic for calling left/right movement function
+        // (affects horizontal movement)
+
+        // when left arrow key is pressed
         if(leftPressed){
             move_horizontal();
+        // when right arrow key is pressed
         } else if (rightPressed) {
             move_horizontal();
+        // when both keys are unpressed but there's momentum
         } else if (horizontal_velocity > 0){
             horizontal_velocity = Math.max(horizontal_velocity - friction, 0);
             move_horizontal();
@@ -165,12 +173,12 @@ $(document).keydown(function(e) {
                 return;
         }
         e.preventDefault();
-    });
+    })
 
 // check for key releases
 $(document).keyup(function(e) {
         switch(e.which) {
-            case 32:
+            case 32: // space key
                 spacePressed = false;
                 jump_counter = 0;
                 break;
@@ -188,6 +196,7 @@ $(document).keyup(function(e) {
         e.preventDefault();
     });
 
+// helpers for detecting events (where circle is)
 function is_on_base_platform(potential_y) {
     return potential_y >= bottom_y_barrier;
 }
@@ -228,7 +237,26 @@ function hit_left_of_small_platform(potential_x, potential_y) {
             y_in_range_of_small_platform(potential_y))
 }
 
+// helper for setting up bounces
+function bounce_helper() {
+    // when we hit platform for the first time after a jump
+    if (!bounce_flag){
+        vertical_velocity = vertical_velocity * -1 * velocity_loss;
+        bounce_flag = true;
+    // subsequent bounces
+    } else if (vertical_velocity > bounce_velocity_threshold){
+        vertical_velocity = vertical_velocity * -1 * velocity_loss;
+    // stop bouncing
+    } else {
+        jump_time = 0;
+        bounce_flag = false;
+        vertical_velocity = 0;
+    }
+}
+
+// function which modifies vertical axis of circle
 function jump_up_and_down() {
+    // apply gravity
     vertical_velocity += gravity * jump_time
 
     // on the way up
@@ -242,6 +270,7 @@ function jump_up_and_down() {
             
             y = top_y_barrier;
             vertical_velocity = vertical_velocity * -1 * velocity_loss;
+
         // if we hit the bottom of the small platform
         } else if (hit_bottom_of_small_platform(x, y+vertical_velocity)){
 
@@ -265,34 +294,17 @@ function jump_up_and_down() {
             }
 
             y = bottom_y_barrier;
-            if (!bounce_flag){
-                vertical_velocity = vertical_velocity * -1 * velocity_loss;
-                bounce_flag = true;
-            } else if (vertical_velocity > 3){
-                vertical_velocity = vertical_velocity * -1 * velocity_loss;
-            } else {
-                jump_time = 0;
-                bounce_flag = false;
-                vertical_velocity = 0;
-            }
-            
+            bounce_helper();
+
+        // if we hit the top of the small platform
         } else if (is_on_small_platform(x, y+vertical_velocity)) {
 
             if (testing) {
                 console.log("hit top of small platform")
             }
 
-            y = small_platform_y - circle_radius
-            if (!bounce_flag){
-                vertical_velocity = vertical_velocity * -1 * velocity_loss;
-                bounce_flag = true;
-            } else if (vertical_velocity > 3){
-                vertical_velocity = vertical_velocity * -1 * velocity_loss;
-            } else {
-                jump_time = 0;
-                bounce_flag = false;
-                vertical_velocity = 0;
-            }
+            y = small_platform_y - circle_radius;
+            bounce_helper();
         } else {
             y += vertical_velocity
         }
@@ -307,12 +319,9 @@ function jump_up_and_down() {
 
 function move_horizontal() {
 
-    // only add acceleration if ball is on a platform
-    // ball on base platform
+    // only add acceleration if circle is on a platform
     if (is_on_base_platform(y)||
-        // ball on small platform
         (is_on_small_platform(x, y))){
-        // add acceleration
         horizontal_velocity *= cur_horizontal_acceleration;
     }
     
@@ -339,7 +348,7 @@ function move_horizontal() {
             x = small_platform_right_x + circle_radius;
             horizontal_velocity = horizontal_velocity * -1 * velocity_loss;
 
-        // rolling off the left
+        // rolling off the left of small platform
         } else if (!is_on_small_platform(x+horizontal_velocity, y)&&
                     is_on_small_platform(x,y)){
 
@@ -347,6 +356,7 @@ function move_horizontal() {
                 console.log("rolling off small platform on left")
             }
 
+            // secret sauce is making the fall a tiny jump
             x = x + horizontal_velocity
             vertical_velocity = -0.1
             jump_up_and_down();
@@ -356,7 +366,7 @@ function move_horizontal() {
         }
         
     }
-    // moving right
+    // moving right (same logic as left but mirrored)
     else{
         // if hitting right side of screen
         if (x + horizontal_velocity >= right_x_barrier){
@@ -392,6 +402,7 @@ function move_horizontal() {
         }
     }
 
+    // change x axis of circle
     circle.attrs({
       cx: x
     });
